61d37065692085275e6743b5341590c1a69ce6da
nikita <nikitaradukan5@gmail.com>
2026-04-22 23:40:32 +0300

    hope final commit

--- database.py
+++ database.py
@@ -1,10 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = "postgresql://postgres:ZQeMLdUSYYpQXweDtgtdcHrEaIUdGUQp@shortline.proxy.rlwy.net:41001/railway"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
