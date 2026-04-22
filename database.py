33d39a33621799d1953eb7f9f74bbdcc92d60b2b
nikita <nikitaradukan5@gmail.com>
2026-04-22 23:36:22 +0300

    fix database connection for deploy

--- database.py
+++ database.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+import os
 
-DATABASE_URL = "postgresql://nikita@localhost/smarthire"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
